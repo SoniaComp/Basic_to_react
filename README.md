7fcef3ac99b96287f8924e0f38fa899e64845c3c
soniacomp <syparkehsc@naver.com>
2020-08-11 17:27:30 +0900

    add_style

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 webpack(bundling의 기능)을 사용하는 이유와 비슷
 
 shadow dom이라는 dom과는 다른 부분에 새로운 dom을 생성해준다.
+
+## 1. 컴포넌트 생성
+
+## 2. 이쁜 UI를 만들기 위해 shadow DOM을 써보장~
--- react-component.js
+++ react-component.js
@@ -1,29 +1,58 @@
 class helloworld extends HTMLElement {
   constructor(){
     super();
+    // add shadow root in constructor
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = `
+      <style>div { background-color: #82b74b; }</style>
+      <div>yey</div>
+    `;
+    // style 정의 및 div 생성
+    this._yey = shadowRoot.querySelector('div');
   }
   static get observedAttributes() {
-    // 모니터링 할 속성 이름
+    return ['lang'];
   }
-  connectedCallback(){
-    // DOMC에 추가되면 실행되는 method;
-    this.innerText = "hi!";
-    this.set();
-  }
-  disconnectedCallback(){
-    // DOM에서 제거되면 실행되는 method;
-  }
-  attributeChangedCallback(attrName, oldVal, newVal){
+  // connectedCallback(){
+  //   // DOMC에 추가되면 실행되는 method;
+  //   this.innerText = "hi!";
+  //   this.set();
+  // }
+  // disconnectedCallback(){
+  //   // DOM에서 제거되면 실행되는 method;
+  // }
+  attributeChangedCallback(attrName, oldVal, newVal) {
     // 속성이 추가/제거/변경되면 실행되는 method
+    // 여기서 lang이라는 attribute가 추가되면 if문에서 통과된다.
+    if (attr == "lang") {
+      let yey;
+      switch (newValue) {
+        case 'ko':
+          yey = '만세';
+          break;
+        case 'es':
+          yey = 'hooray!';
+          break;
+        case 'jp':
+          yey = '万歳!';
+          break;
+        default:
+          yey = 'yey!';
+      }
+      this._yey.innerText = yey;
+    }
   }
-  adoptedCallback(oldDoc, newDoc){
-    // 다른 Document에서 옮겨지면 실행되는 method
+  yell() {
+    alert(this._yey.innerText);
   }
-  set(){
-    setTimeout(()=>{
-      this.innerText = "my name is indio";
-    }, 1000)
-  }
+  // adoptedCallback(oldDoc, newDoc) {
+  //   // 다른 Document에서 옮겨지면 실행되는 method
+  // }
+  // set() {
+  //   setTimeout(() => {
+  //     this.innerText = "my name is indio";
+  //   }, 1000)
+  // }
 }
 
 customElements.define('hello-world', helloworld);
